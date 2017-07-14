8a27de13d83f697db6fe74e584b8824b0763c0e0
wendf <fengGe_wen@163.com>
2017-07-14 09:29:29 +0800

    配置项目

---
+++ MyWeibo/MyWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyWeibo
+//
+//  Created by wendf on 2017/7/14.
+//  Copyright © 2017年 HMYD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
