e684c4b3283c8896f9eac1226c25d527aeb2b319
wendf <fengGe_wen@163.com>
2017-07-14 10:03:16 +0800

    11

---
+++ MyWeibo/MyWeibo/Classes/View/Main/WBBaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WBBaseViewController.swift
+//  MyWeibo
+//
+//  Created by wendf on 2017/7/14.
+//  Copyright © 2017年 HMYD. All rights reserved.
+//
+
+import UIKit
+
+class WBBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       view.backgroundColor = UIColor.cz_random()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyWeibo/MyWeibo/Weibo-Bridge.h
@@ -0,0 +1,11 @@
+//
+//  Weibo-Bridge.h
+//  MyWeibo
+//
+//  Created by wendf on 2017/7/14.
+//  Copyright © 2017年 HMYD. All rights reserved.
+//
+
+//桥接文件，专门用于引用 OC 的头文件 ，一旦引入，swift就可以正常使用（宏除外）
+
+#import "CZAdditions.h"
